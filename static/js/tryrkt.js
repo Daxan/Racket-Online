95484781feff56bce46bd6d89629b4620d798b53
Daxan <chrakhan@yahoo.de>
2014-11-27 15:25:17 +0100

    evalUrl

--- static/js/tryrkt.js
+++ static/js/tryrkt.js
@@ -73,7 +73,7 @@ function eval_racket(code) {
 
 	var data;
 	$.ajax({
-		url: evalUrl ,
+		url: evalUrl,
 		data: {expr: code},
 		async: false,
 		success: function(res) { $("#some").html(data); data = res; },
