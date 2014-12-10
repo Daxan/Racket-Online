fe2db5acc41445d2119e2a2bfd6fabfd2fe19c91
Daxan <chrakhan@yahoo.de>
2014-12-10 17:02:30 +0100

    m

--- static/js/lastupdate.js
+++ static/js/lastupdate.js
@@ -3,13 +3,13 @@ function write_last_mod ()
     var loc = new String (document.location);
     if (loc.match (/\.html($|\?.*)|\/$/)) {
         var last_modified = new Date (document.lastModified);
-        var date_stamp = "<p><small>This page was ";
+        var date_stamp = "<p><small>letzte Modifikation";
         if (typeof (created) != 'undefined') {
             date_stamp += " created on <b>"
                        + created.toDateString ()
                        + "</b> and ";
         }
-        date_stamp +=    "last changed on <b>"
+        date_stamp +=    ": <b>"
                        + last_modified.toDateString ()
                        + "</b>.</small></p>";
         document.writeln (date_stamp);
